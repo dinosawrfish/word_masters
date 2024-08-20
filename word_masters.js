98f029b15f462d5fb4449394bbe16dec444e3706
dinosawrfish <dinosawrfish@gmail.com>
2024-08-20 14:39:06 -0700

    fetch get word of the day and process response

--- word_masters.js
+++ word_masters.js
@@ -1,5 +1,21 @@
+const WORD_OF_DAY_URL = "https://words.dev-apis.com/word-of-the-day";
+
+function getWordOfDay() {
+    const promise = fetch(WORD_OF_DAY_URL);
+    promise
+        .then(function (response) {
+            const processingPromise = response.json();
+            return processingPromise;
+        })
+        .then(function (processedResponse) {
+            const word = processedResponse.word;
+            console.log(word);
+        })
+}
+
 function init() {
     // TODO: get word of the day when page loads
+    const wordOfDay = getWordOfDay();
 }
 
 init()
