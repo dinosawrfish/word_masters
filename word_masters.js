b6dde2c08a4a4c5608b729be0b8d58622f301a88
dinosawrfish <dinosawrfish@gmail.com>
2024-08-20 15:38:15 -0700

    return word not promise

--- word_masters.js
+++ word_masters.js
@@ -3,14 +3,13 @@ const WORD_OF_DAY_URL = "https://words.dev-apis.com/word-of-the-day";
 async function getWordOfDay() {
     const promise = await fetch(WORD_OF_DAY_URL);
     const processedResponse = await promise.json();
-    const wordOfDay = processedResponse.word;
-
-    return wordOfDay;
+    return processedResponse.word;
 }
 
-function init() {
+async function init() {
     // TODO: get word of the day when page loads
-    const wordOfDay = getWordOfDay();
+    const wordOfDay = await getWordOfDay()
+
 }
 
 init()
